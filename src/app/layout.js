b273e18f921bc2eae28820318ce8615d8095fb42
ajoxales <ajoxales@gmail.com>
2023-07-29 00:20:24 +0800

    for deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,9 @@
-"use client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Inter, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import NavbarComp from "@/components/NavbarComp";
 import Footer from "@/components/Footer";
 import "../app/globals.css";
-import Head from "next/head";
+
 const poppins = Poppins({
 	subsets: ["latin"],
 	weight: ["300", "400", "500", "600", "700"],
