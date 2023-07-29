29c403baf3831fb8a923f648000a897b8947b453
ajoxales <ajoxales@gmail.com>
2023-07-30 01:08:00 +0800

    minor fixes

--- src/app/projects/calculator/page.js
+++ src/app/projects/calculator/page.js
@@ -4,10 +4,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import HeaderComponent from "@/app/projects/calculator/components/HeaderComponent";
 import Calculator from "@/app/projects/calculator/components/Calculator";
+import styles from "@/app/projects/calculator/styles/styles.module.css";
 
 const Page = () => {
 	return (
-		<div>
+		<div className={styles.className}>
 			<HeaderComponent />
 			<Calculator />
 		</div>
